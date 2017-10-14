d919da7bf7074189ad6cb9bacd0bf709edbd11e6
mrjobosco <mrjobosco@gmail.com>
2017-10-14 17:22:22 +0100

    link DB

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 
 export default () => {
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://heroku_zn9jzq8q:Abc123cd@ds137207.mlab.com:37207/meetupME')
-    mongoose.connection
+    mongoose.connect('mongodb://test:test@ds137207.mlab.com:37207/heroku_zn9jzq8q')
+    mongoose.connection    
     .once('open', () => console.log('mongoDB Running'))
     .on('error', (err)=>{
         console.log(`${err}`)
