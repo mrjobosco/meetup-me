eea891712671d71ba5f3771fbd1a848c9ca7a00f
mrjobosco <mrjobosco@gmail.com>
2017-10-14 15:58:36 +0100

    Changed DB

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export default () => {
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/meetupME')
+    mongoose.connect('mongodb://heroku_zn9jzq8q:Abc123cd@ds137207.mlab.com:37207/meetupME')
     mongoose.connection
     .once('open', () => console.log('mongoDB Running'))
     .on('error', (err)=>{
